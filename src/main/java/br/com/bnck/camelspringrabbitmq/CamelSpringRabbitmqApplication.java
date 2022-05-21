41ec5345470c175e2389a4f313ef3d24c96471d9
Thiago Moreira Bianeck <thgbianeck@gmail.com>
2022-05-21 20:49:35 -0300

    Commit Inicial

---
+++ src/main/java/br/com/bnck/camelspringrabbitmq/CamelSpringRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package br.com.bnck.camelspringrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSpringRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelSpringRabbitmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/camelspringrabbitmq/CamelSpringRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camelspringrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelSpringRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
